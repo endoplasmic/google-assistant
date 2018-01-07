69e780dd62ddba0ba7e73916afefddf75d8b59bd
Scott Ysebert <endoplasmic@gmail.com>
2018-01-06 22:51:54 -0800

    :zap: - config now has 2 sections, `auth` and `conversation`.

--- index.js
+++ index.js
@@ -7,21 +7,34 @@ const Assistant = require('./components/assistant');
 const Auth = require('./components/auth');
 const Conversation = require('./components/conversation');
 
-function GoogleAssistant(config) {
-  if (config === undefined) config = {};
+function GoogleAssistant(authConfig, callback) {
+  if (authConfig === undefined) {
+    const error = new Error('Missing auth config object!');
+    this.emit('error', error);
+    if (callback) callback(error);
+    return;
+  }
 
   let assistant;
 
   // we need to auth with Google right out of the gate
-  const auth = new Auth(config.auth);
+  const auth = new Auth(authConfig);
 
   auth.on('ready', (client) => {
     assistant = new Assistant(client);
     this.emit('ready', assistant);
+    if (callback) callback(assistant);
   });
 
-  this.start = (callback) => {
-    const conversation = new Conversation(assistant, config);
+  this.start = (conversationConfig, callback) => {
+    if (assistant === undefined) {
+      const error = new Error('Tried calling start() before the ready event!');
+      this.emit('error', error);
+      if (callback) callback(error);
+      return;
+    }
+
+    const conversation = new Conversation(assistant, conversationConfig);
     this.emit('started', conversation);
     if (callback) callback(conversation);
   };
