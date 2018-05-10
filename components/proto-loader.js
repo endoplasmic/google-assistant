2351d08e77ab6f1d3a389e1e8e59176959f037e4
Scott Ysebert <endoplasmic@gmail.com>
2018-05-09 20:54:13 -0700

    :bug: - Possibly fixed a bug with protobuf's not resolving to the right directory

--- components/proto-loader.js
+++ components/proto-loader.js
@@ -4,7 +4,7 @@ const protobuf = require('protobufjs');
 const protoRoot = new protobuf.Root();
 protoRoot.resolvePath = (origin, target) => {
   // we need to resolve each import to the lib directory instead of this one
-  return protobuf.util.path.resolve(origin, path.resolve('../lib', target));
+  return protobuf.util.path.resolve(origin, path.resolve(__dirname, '../lib', target));
 };
 
 module.exports = protoRoot;
