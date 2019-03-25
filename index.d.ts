9d8fda99ea0b8ae66f02f2784097f7604e9747ed
John <--remove>
2019-03-25 23:18:12 +0000

    typescript type updates

--- index.d.ts
+++ index.d.ts
@@ -1,58 +1,81 @@
 // Type definitions for google-assistant v0.5.3
+type ConversationEventType = "end-of-utterance" | "transcription" | "audio-data" | "device-action" | "response" | "volume-percent" | "screen-data" | "error" | "ended";
+type AssistantEventType = "ready" | "error" | "started";
+type ConversationHandlerCallback = () => void;
 
-type AuthConfig = {
-    keyFilePath: string;
-    savedTokensPath: string;
-    tokenInput?(processTokens: any): void;
-};
-
-type AudioConfig = {
-    encodingOut?: "LINEAR16" | "MP3" | "OPUS_IN_OGG";
-    sampleRateOut?: number;
-    encodingIn?: "LINEAR16" | "FLAC";
-    sampleRateIn?: number;
-};
-
-type ConversationConfig = {
-    audio?: AudioConfig;
-    textQuery?: string;
-    deviceId?: string;
-    deviceModelId?: string;
-    lang?: string;
-    isNew?: boolean;
-    deviceLocation?: DeviceLocation;
-    screen?: ScreenConfig;
-};
-
-type DeviceLocation = {
-    coordinates: { latitude: number; longitude: number };
-};
-
-type ScreenConfig = { isOn: boolean };
-
-declare class EventEmitter {
-    public emit(type: string, payload?: any): any;
-    public on(type: string, handler: Function): any;
+declare class Assistant {
+    converse(): GoogleAssistant.Conversation;
 }
 
-declare class Conversation extends EventEmitter {
-    constructor(config: ConversationConfig);
-    public write(bytes: Uint8Array): void;
-    public end(): void;
-}
+declare namespace GoogleAssistant {
+    namespace Config {
+        export type AudioConfig = {
+            encodingOut?: "LINEAR16" | "MP3" | "OPUS_IN_OGG";
+            sampleRateOut?: number;
+            encodingIn?: "LINEAR16" | "FLAC";
+            sampleRateIn?: number;
+        };
 
-declare class Assistant {}
+        export type ScreenConfig = { isOn: boolean };
 
-declare class GoogleAssistant extends EventEmitter {
-    constructor(
-        authConfig: AuthConfig,
-        callback?: (callbackArg: Assistant | Error) => void
-    );
+        export type AuthConfig = {
+            keyFilePath: string;
+            savedTokensPath: string;
+            tokenInput?(processTokens: any): void;
+        };
 
-    public start(
-        conversation?: ConversationConfig,
-        callback?: (callbackArg: Conversation | Error) => void
-    ): void;
+        export type ConversationConfig = {
+            audio?: AudioConfig;
+            textQuery?: string;
+            deviceId?: string;
+            deviceModelId?: string;
+            lang?: string;
+            isNew?: boolean;
+            deviceLocation?: DeviceLocationConfig;
+            screen?: ScreenConfig;
+        };
+
+        export type DeviceLocationConfig = {
+            coordinates: { latitude: number; longitude: number };
+        };
+    }
+
+    class AssistantEventEmitter {
+        private emit(type: AssistantEventType, payload?: any): any;
+        on(type: "ready", handler: (assistant: GoogleAssistant) => void): GoogleAssistant;
+        on(type: "started", handler: (conversation: Conversation, assistant: GoogleAssistant) => void): GoogleAssistant;
+        on(type: "error", handler: (error: Error) => void): GoogleAssistant;
+        on(type: AssistantEventType, handler: (value: any) => void): GoogleAssistant;
+    }
+
+    class ConversationEventEmitter {
+        private emit(type: ConversationEventType, payload?: any): any;
+        on(type: "ended", handler: (error: Error, continueConversation: boolean) => void): Conversation;
+        on(type: "volume-percent", handler: (percent: number) => void): Conversation;
+        on(type: "audio-data", handler: (audioData: any) => void): Conversation;
+        on(type: "transcription", handler: (transcript: { transcription: string, done: boolean }) => void): Conversation;
+        on(type: "error", handler: (error: Error) => void): Conversation;
+        on(type: AssistantEventType, handler: (value: any) => void): Conversation;
+    }
+
+    export class Conversation extends ConversationEventEmitter {
+        constructor(config: Config.ConversationConfig);
+        write(bytes: Uint8Array): void;
+        end(): void;
+    }
+
+    export class GoogleAssistant extends AssistantEventEmitter {
+        constructor(
+            authConfig: Config.AuthConfig,
+            callback?: (callbackArg: Assistant | Error) => void
+        );
+
+        start(
+            conversation?: Config.ConversationConfig,
+            callback?: (callbackArg: Conversation | Error) => void
+        ): void;
+    }
+
 }
 
 export = GoogleAssistant;
